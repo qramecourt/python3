b942c2d1c957333e359778198f5c91fe891b9510
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-03-02 12:11:16 +0100

    création d'un repo

---
+++ variables.py
@@ -0,0 +1,40 @@
+number = 15
+print(number)
+
+number = 12
+print(number)
+
+number2 = 3.1416
+
+divide = number / number2
+
+
+text = "foo  bar "
+yes_no = True
+print(text  + str(divide) + " " + str(yes_no))
+yes_no = False
+print(text  + str(divide) + " "+ str(yes_no))
+#mais c'est quoI ce truc?
+nickname = "brian o\' conner"
+print(nickname)
+html_code = '<h1>coucou, tu veux voir ma bite?</h1>'
+print(html_code)
+text = """foo
+bar
+baz"""
+print(text)
+print(type(text))
+#conversion
+nombre =3.14
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+
+# my_var_ = "-123"
+
+# my_var_= bool(my_var_)
+# print(my_var_)
+
